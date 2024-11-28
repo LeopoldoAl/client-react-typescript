7c51614683a36ed1b07e2d5ac52558d4a93c3c5a
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-28 15:53:09 -0400

    client-phase 15

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Products from './views/Products'
-import LayoutEcommerce from './layouts/LayoutEcommerce'
-import Ecommerce from './views/Ecommerce'
 import NewProduct from './views/NewProduct'
 
 export const router = createBrowserRouter([
@@ -20,14 +18,4 @@ export const router = createBrowserRouter([
             }
         ],
     },
-    {
-        path: '/ecommerce',
-        element: <LayoutEcommerce />,
-        children: [
-            {
-                index: true,
-                element: <Ecommerce/>
-            },
-        ]
-    }
 ])
