a7010ccd8331bb1c0d0d71865aebe37430a69801
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-11-29 09:35:13 -0400

    client-phase 17

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,7 +1,19 @@
-
+import { Link } from 'react-router-dom'
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className='text-4xl font-black text-slate-500'>
+          Products
+        </h2>
+        <Link
+          to="product/new"
+          className='rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500'
+        >
+          Adding Product
+        </Link>
+      </div>
+    </>
   )
 }
