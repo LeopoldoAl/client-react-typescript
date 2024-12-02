d303ede1ba38ddd561bfcaa9e635147426ce1f9f
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-02 12:17:03 -0400

    client-phase 42

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -7,6 +7,7 @@ type ProductDetailsProps = {
 }
 
 export default function ProductDetails({product}: ProductDetailsProps) {
+    const navigate = useNavigate()
     const isAvailable = product.availability
   return (
     <tr className="border-b ">
@@ -21,12 +22,12 @@ export default function ProductDetails({product}: ProductDetailsProps) {
         </td>
         <td className="p-3 text-lg text-gray-800 ">
             <div className="flex gap-2 items-center">
-                <Link
-                    to={`/products/${product.id}/edit`}
+                <button
+                    onClick={() => navigate(`/products/${product.id}/edit`)}
                     className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
                 >
                     Edit
-                </Link>
+                </button>
             </div>
         </td>
     </tr> 
