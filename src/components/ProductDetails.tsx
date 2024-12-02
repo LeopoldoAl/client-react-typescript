b3ff288a43c312f25253906b107508d9cfab0fee
Leopoldo <leopoldoalvaradogomez@gmail.com>
2024-12-02 12:07:20 -0400

    client-phase 41

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -20,9 +21,12 @@ export default function ProductDetails({product}: ProductDetailsProps) {
         </td>
         <td className="p-3 text-lg text-gray-800 ">
             <div className="flex gap-2 items-center">
-                <button>
+                <Link
+                    to={`/products/${product.id}/edit`}
+                    className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                >
                     Edit
-                </button>
+                </Link>
             </div>
         </td>
     </tr> 
